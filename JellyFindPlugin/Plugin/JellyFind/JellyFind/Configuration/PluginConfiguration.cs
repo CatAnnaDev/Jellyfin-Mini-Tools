c721fe360238369c1e6ce4b20957cee859e2d0c8
Anna <45910905+CatAnnaDev@users.noreply.github.com>
2025-05-13 09:11:05 +0200

    Add files via upload

---
+++ JellyFindPlugin/Plugin/JellyFind/JellyFind/Configuration/PluginConfiguration.cs
@@ -0,0 +1,23 @@
+using MediaBrowser.Model.Plugins;
+
+namespace JellyFind.Configuration;
+
+/// <summary>
+/// Plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // set default options here
+        AString = "string";
+    }
+
+    /// <summary>
+    /// Gets or sets a string setting.
+    /// </summary>
+    public string AString { get; set; }
+}
